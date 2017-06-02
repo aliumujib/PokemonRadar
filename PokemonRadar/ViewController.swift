d37eecaa9f6c2e23cb0a97665b19c70dbdfece06
aliumujib <aliuabdulmujib@gmail.com>
2017-06-02 23:51:19 +0100

    Initial Commit

---
+++ PokemonRadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonRadar
+//
+//  Created by Abdul-Mujib Aliu on 6/2/17.
+//  Copyright © 2017 Abdul-Mujib Aliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
